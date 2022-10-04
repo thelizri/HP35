a6a4920985cb6f67cc2bf0bd0eba001ee87c5e6c
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-10-04 23:28:20 +0200

    Dynamic Circular Queues

--- HP35/Program.cs
+++ HP35/Program.cs
@@ -9,7 +9,32 @@ namespace HP35
     {
         static void Main()
         {
-           
+            var queue = new CircularDynamicQueue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                queue.add(i);
+            }
+            queue.print();
+            for (int i = 0; i < 10; i++)
+            {
+                queue.remove();
+            }
+            for (int i = 10; i < 30; i++)
+            {
+                queue.add(i);
+            }
+            queue.print();
+            
+            for (int i = 10; i < 30; i++)
+            {
+                queue.remove();
+            }
+            for (int i = 30; i < 34; i++)
+            {
+                queue.add(i);
+            }
+            Console.WriteLine();
+            queue.print();
         }
         
     }
