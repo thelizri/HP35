38ceac65966a9e3a4fd2539ae0513a0229116238
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-10-11 16:43:14 +0200

    TreeHeap LinkedList implementation

--- HP35/Current/TreeHeap.cs
+++ HP35/Current/TreeHeap.cs
@@ -61,6 +61,82 @@ public class TreeHeap
     {
         if (root is null)
             throw new Exception("Heap is empty");
-        return root.data;
+        if (root.size == 1)
+        {
+            int result = root.data;
+            root = null;
+            return result;
+        }
+        else
+        {
+            int result = root.data;
+            remove(root);
+            return result;
+        }
     }
+
+    private void remove(Node node)
+    {
+        node.size--;
+        if (node.left is null)
+        {
+            //Promote right branch
+            promote(node, node.right);
+            if (isLeaf(node.right))
+            {
+                node.right = null;
+                return;
+            }
+            remove(node.right);
+        }
+        else if (node.right is null)
+        {
+            //Promote left branch
+            promote(node, node.left);
+            if (isLeaf(node.left))
+            {
+                node.left = null;
+                return;
+            }
+            remove(node.left);
+        }
+        else if (node.left.data <= node.right.data)
+        {
+            //Promote left branch
+            promote(node, node.left);
+            if (isLeaf(node.left))
+            {
+                node.left = null;
+                return;
+            }
+            remove(node.left);
+        }
+        else if (node.right.data < node.left.data)
+        {
+            //Promote right branch
+            promote(node, node.right);
+            if (isLeaf(node.right))
+            {
+                node.right = null;
+                return;
+            }
+            remove(node.right);
+        }
+    }
+
+    private void promote(Node node, Node promote)
+    {
+        node.data = promote.data;
+    }
+
+    private bool isLeaf(Node node)
+    {
+        if (node.left is null && node.right is null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    
 }
