d202b89b379eb58a2683e44bb40a54db72433d84
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-10-13 15:56:50 +0200

    dddd

---
+++ HP35/Current/Hash/Zip.cs
@@ -0,0 +1,42 @@
+﻿namespace HP35.Current.Hash;
+
+public class Zip
+{
+    private Node[] data;
+    private readonly string fileAddress;
+    private readonly string directory;
+
+    private class Node
+    {
+        private String code;
+        private String name;
+        private int pop;
+
+        public Node(string code, string name, int pop)
+        {
+            this.code = code;
+            this.name = name;
+            this.pop = pop;
+        }
+    }
+
+    public Zip(string file)
+    {
+        this.data = new Node[10000];
+        this.directory = "C:\\Users\\karlw\\Documents\\Code\\C#\\HP35\\HP35\\Current\\Hash\\";
+        this.fileAddress = directory + file;
+    }
+
+    public void read()
+    {
+        if (!File.Exists(fileAddress)) return;
+        string[] lines = File.ReadAllLines(fileAddress);
+        int i = 0;
+        foreach (string line in lines)
+        {
+            string[] row = line.Split(' ');
+            Console.WriteLine($"First: {row[0]}, Second: {row[1]}, Third: {row[2]},");
+            data[i++] = new Node(row[0], row[1], Int32.Parse(row[2]));
+        }
+    }
+}
--- HP35/Program.cs
+++ HP35/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Diagnostics;
 using HP35.Current;
+using HP35.Current.Hash;
 using Microsoft.VisualBasic.CompilerServices;
 
 namespace HP35
@@ -10,86 +11,8 @@ namespace HP35
     {
         static void Main()
         {
-            measure_time(false);
-        }
-
-        
-        static void measure_time(bool sigfigs)
-        {
-            int n = 10;
-            int numberOfTests = 12; //n doubles every test
-            int outerLoop = 1000;
-            int innerLoop = 1000;
-            double difference = 0;
-            double previous = 0;
-    
-                
-            Random random = new Random();
-    
-            for (int i = 0; i < numberOfTests; i++)
-            {
-                double sum = 0;
-                double min = Double.PositiveInfinity;
-                double max = 0;
-                double mean = 0;
-
-                //Improve performance for large numbers
-                if (n >= 30000) outerLoop = 100;
-                if (n >= 1000000) outerLoop = 25;
-                    
-                for (int j = 0; j < outerLoop; j++)
-                {
-                    //Prep work
-                    var heap = arrayHeap(n);
-                    var data = ArrayTools.create_unsorted_array(innerLoop);
-    
-                    //Measure the time
-                    long t0 = Stopwatch.GetTimestamp();
-                    for (int ii = 0; ii < innerLoop; ii++)
-                    {
-                            heap.add(data[ii]);
-                            heap.remove();
-                    }
-                    long t1 = Stopwatch.GetTimestamp();
-                    double time = (t1 - t0);
-                        
-                    //Stats
-                    time /= innerLoop;
-                    sum += time;
-                    max = Math.Max(max, time);
-                    min = Math.Min(min, time);
-                }
-                
-                mean = sum / outerLoop;
-                difference = mean - previous;
-                previous = mean;
-                Latex.addLine(n,mean,min,max,sigfigs);
-    
-                n *= 2;
-            }
-            Latex.print();
-        }
-
-        static TreeHeap treeHeap(int n)
-        {
-            Random r = new Random();
-            var heap = new TreeHeap();
-            for (int i = 0; i < n; i++)
-            {
-                heap.add(r.Next(100*n));
-            }
-            return heap;
-        }
-        static ArrayHeap arrayHeap(int n)
-        {
-            Random r = new Random();
-            var heap = new ArrayHeap();
-            for (int i = 0; i < n; i++)
-            {
-                heap.add(r.Next(100*n));
-            }
-
-            return heap;
+            Zip zip = new Zip("NewFile1.txt");
+            zip.read();
         }
     }
 }
